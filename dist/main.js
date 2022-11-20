0dc1e757e7eb82ddb20b1e274ef82f3bf7436cc8
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2022-11-20 18:41:37 +0100

    Update 3
    Added responsive navbar

---
+++ dist/main.js
@@ -0,0 +1,47 @@
+// Mobile hamburger menu nodes
+
+const menuBtn = document.querySelector('.header__menu');
+const hamburger = document.querySelector('.menu__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.nav__ul');
+const navHome = document.querySelector('#home');
+const navAbout = document.querySelector('#about');
+const navPlans = document.querySelector('#plans');
+const navContact = document.querySelector('#contact');
+
+// Addition of 'open' class that toggles 
+// navbar to pop up
+
+let openMenu = false;
+
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu () {
+    if(!openMenu){
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        openMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        openMenu = false;
+    }
+};
+
+// Function that hides navbar when
+// links are clicked
+
+navHome.addEventListener('click', closeMenu);
+navAbout.addEventListener('click', closeMenu);
+navPlans.addEventListener('click', closeMenu);
+navContact.addEventListener('click', closeMenu);
+
+function closeMenu () {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    openMenu = false;
+};
